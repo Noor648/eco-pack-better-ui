4c71135571589de3f5acb54c7ae61b782abfdbda
Noor Ul Ain <noornasim908@gmail.com>
2025-02-15 15:37:54 +0500

    Added collection type listing page

--- data.tsx
+++ data.tsx
@@ -8,15 +8,15 @@ export const navLinks = [
 ];
 
 export const category = [
-  { name: 'Customized Printed Sticker', image: '/images/category-1.webp', href: '/categories/skincare' },
-  { name: 'Restaurant & Cafes', image: '/images/category-2.webp', href: '/categories/skincare' },
-  { name: 'Birchwood Cutlery', image: '/images/category-3.webp', href: '/categories/skincare' },
-  { name: 'Bakery and Cake', image: '/images/category-4.webp', href: '/categories/skincare' },
-  { name: 'Salad & Rice Bowls', image: '/images/category-1.webp', href: '/categories/skincare' },
-  { name: 'Paper Bags', image: '/images/category-2.webp', href: '/categories/skincare' },
-  { name: 'Ecommerce Boxes', image: '/images/category-3.webp', href: '/categories/skincare' },
-  { name: 'Kraft Bowls', image: '/images/category-4.webp', href: '/categories/skincare' },
-  { name: 'Coffee/Tea Cafe', image: '/images/category-1.webp', href: '/categories/skincare' },
+  { name: 'Customized Printed Sticker', image: '/images/category-1.webp', href: '/collections/listing' },
+  { name: 'Restaurant & Cafes', image: '/images/category-2.webp', href: '/collections/listing' },
+  { name: 'Birchwood Cutlery', image: '/images/category-3.webp', href: '/collections/listing' },
+  { name: 'Bakery and Cake', image: '/images/category-4.webp', href: '/collections/listing' },
+  { name: 'Salad & Rice Bowls', image: '/images/category-1.webp', href: '/collections/listing' },
+  { name: 'Paper Bags', image: '/images/category-2.webp', href: '/collections/listing' },
+  { name: 'Ecommerce Boxes', image: '/images/category-3.webp', href: '/collections/listing' },
+  { name: 'Kraft Bowls', image: '/images/category-4.webp', href: '/collections/listing' },
+  { name: 'Coffee/Tea Cafe', image: '/images/category-1.webp', href: '/collections/listing' },
 ];
 
 export const categories = [
@@ -112,6 +112,130 @@ export const products = [
     price: 'Rs 56.00',
     originalPrice: 'Rs 70.00',
     isOnSale: true,
+  },
+];
+
+// product Listing screen
+
+export const productsList = [
+  {
+    id: 1,
+    image: '../images/product-1.webp',
+    name: 'Corrugated Flat Box - 12.5" x 10.5" x 2" Inches',
+    price: 'Rs 56.00',
+    originalPrice: 'Rs 70.00',
+    isOnSale: true,
+  },
+  {
+    id: 2,
+    image: '../images/product-2.webp',
+    name: 'Corrugated Frame Box',
+    price: 'Rs 50.00',
+    originalPrice: 'Rs 58.00',
+    isOnSale: true,
+  },
+  {
+    id: 3,
+    image: '../images/product-3.webp',
+    name: 'Corrugated Flat Box',
+    price: 'Rs 56.00',
+    originalPrice: 'Rs 70.00',
+    isOnSale: false,
+  },
+  {
+    id: 4,
+    image: '../images/product-4.webp',
+    name: 'Corrugated Frame Box',
+    price: 'Rs 50.00',
+    originalPrice: 'Rs 58.00',
+    isOnSale: true,
+  },
+  {
+    id: 5,
+    image: '../images/product-5.webp',
+    name: 'Corrugated Flat Box',
+    price: 'Rs 56.00',
+    originalPrice: 'Rs 70.00',
+    isOnSale: true,
+  },
+  {
+    id: 6,
+    image: '../images/category-1.webp',
+    name: 'Corrugated Flat Box - 12.5" x 10.5" x 2" Inches',
+    price: 'Rs 56.00',
+    originalPrice: 'Rs 70.00',
+    isOnSale: true,
+  },
+  {
+    id: 7,
+    image: '../images/category-2.webp',
+    name: 'Corrugated Frame Box',
+    price: 'Rs 50.00',
+    originalPrice: 'Rs 58.00',
+    isOnSale: true,
+  },
+  {
+    id: 8,
+    image: '../images/category-3.webp',
+    name: 'Corrugated Flat Box',
+    price: 'Rs 56.00',
+    originalPrice: 'Rs 70.00',
+    isOnSale: false,
+  },
+  {
+    id: 9,
+    image: '../images/category-4.webp',
+    name: 'Corrugated Frame Box',
+    price: 'Rs 50.00',
+    originalPrice: 'Rs 58.00',
+    isOnSale: true,
+  },
+];
+
+export const filters = [
+  {
+    title: 'Product Type',
+    content: (
+      <ul className="space-y-2">
+        <li>
+          <input type="checkbox" /> Bakery Boxes (2)
+        </li>
+        <li>
+          <input type="checkbox" /> Disposable Cutlery (1)
+        </li>
+        <li>
+          <input type="checkbox" /> Other (1)
+        </li>
+        <li>
+          <input type="checkbox" /> Packing Materials (3)
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: 'Availability',
+    content: (
+      <ul className="space-y-2">
+        <li>
+          <input type="checkbox" /> In stock (79)
+        </li>
+        <li>
+          <input type="checkbox" /> Out of stock (52)
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: 'Price',
+    content: (
+      <div>
+        <input type="range" min="0" max="7400" className="w-full" />
+        <div className="flex justify-between text-sm text-gray-600">
+          <span>Rs. 0.00</span>
+          <span>Rs. 7,400.00</span>
+        </div>
+      </div>
+    ),
   },
 ];
 
