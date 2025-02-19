94797c14001dec86d777c06a99fd077d7e277b00
Noor Ul Ain <noornasim908@gmail.com>
2025-02-19 16:49:23 +0500

    Added responsive version for product cards

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,7 +12,7 @@ type ProductProps = {
 
 export default function Product({ imageSrc, name, isOnSale = false, originalPrice, price }: ProductProps) {
   return (
-    <Link className="relative group" href={'/product'}>
+    <Link className="relative group block" href={'/product'}>
       {/* Sale Badge */}
       {isOnSale && <span className="absolute left-0 bg-red-500 text-white text-xs py-1 px-2 z-5">SALE</span>}
 
@@ -20,12 +20,25 @@ export default function Product({ imageSrc, name, isOnSale = false, originalPric
       <div className="relative">
         <img src={imageSrc} alt={name} className="w-full h-[210px] object-cover mb-3" />
 
-        {/* Hover Icons */}
-        <div className="absolute inset-0 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/40">
-          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200">
+        {/* Desktop Hover Icons */}
+        <div
+          className="absolute inset-0 hidden sm:flex items-center justify-center gap-2 
+          opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/40"
+        >
+          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200 shadow-md">
             <FaSearch />
           </button>
-          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200">
+          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200 shadow-md">
+            <FaPlus />
+          </button>
+        </div>
+
+        {/* Mobile Fixed Icons */}
+        <div className="absolute top-0 right-10 translate-x-1/2 translate-y-1/2 flex gap-2 sm:hidden">
+          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200 shadow-md">
+            <FaSearch />
+          </button>
+          <button className="p-2 bg-white text-black rounded-full hover:bg-gray-200 shadow-md">
             <FaPlus />
           </button>
         </div>
